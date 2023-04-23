bd5f9d673ed8583f2ac520d1e60cc37fe08334b3
tzhangm <tzhangm@trip.com>
2023-04-23 22:19:20 +0800

    upd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('frontEndConfig')
+  frontEndConfig(): any {
+    return {
+      sentryDsn:
+        'https://89265a61f3e14ea9b79e385b298e670c@o4505063820034048.ingest.sentry.io/4505063821541376',
+    };
   }
 }
